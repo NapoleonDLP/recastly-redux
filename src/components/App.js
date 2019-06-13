5946bde2a6a2380219772b6520918b1c5d97f0a3
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-12 23:30:58 -0700

    Loaded dependencies and Adjusts changeVideo

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';
+// import Reduc from 'redux';
 
 class App extends React.Component {
   constructor(props) {
